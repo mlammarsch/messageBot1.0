0c73ae937b834dfaf8e2e18402cce7defac3938c
Markus Lammarsch <mlammars@gmail.com>
2024-11-24 08:50:46 +0100

    Icons der Anwendung getauscht

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/img/icons/android-chrome-192x192.png',
+            src: '/img/icons/chat-bot_11773927.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/img/icons/android-chrome-512x512.png',
+            src: '/img/icons/chat-bot_11773927.png',
             sizes: '512x512',
             type: 'image/png'
           }
